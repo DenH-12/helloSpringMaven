98d14c585d3b5af8b6c1ee6a97e9229f4f96f53e
Denys <denyshelich12@gmail.com>
2025-07-18 08:18:01 +0300

    Initial commit

---
+++ src/test/java/com/example/HelloSpringMaven/HelloSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloSpringMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
